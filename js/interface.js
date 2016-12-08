245c6bb1064dd299c087c40943e2ec90c72e0f74
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-08 14:36:18 +0000

    clicking plus button makes temp visibly go up

--- js/interface.js
+++ js/interface.js
@@ -1,4 +1,11 @@
 $( document ).ready(function(){
   var thermostat = new Thermostat();
+
   $('#display').text(thermostat.degrees);
+
+  $('#temp-up').click(function() {
+    thermostat.increase();
+    $('#display').text(thermostat.degrees);
+  });
+
 });
